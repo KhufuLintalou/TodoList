8626ab48524e06d6e08fb55082fd5290ece5dfba
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2025-03-26 21:02:39 +0700

    Added functionality to the "+" button that takes input and add the new todo to the selected project and some other changes.

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,5 @@
 import { Project } from "./todoList.js";
+import { getCurrentDate } from "./Date.js";
 
 const Projects = [];
 
@@ -8,12 +9,14 @@ function createProject(name) {
 }
 
 createProject("My Project");
-Projects[0].createTodo("test", "this is for testing", "implement later", 3, "");
+Projects[0].createTodo("test", "this is for testing", "implement later", 3);
+Projects[0].selectProject();
 
 const sidebar = document.querySelector(".sidebar");
 const todoSection = document.querySelector(".todo-section");
-const newTodo = document.querySelector(".new-todo");
-const newProjectButton = document.querySelector("#new");
+const newTodoButtonDiv = document.querySelector(".todo-section > div");
+const newProjectButton = document.getElementById("new-project-button");
+const newTodoButton = document.getElementById("new-todo-button");
 
 function updateDisplay(display) {
     switch (display) {
@@ -23,8 +26,14 @@ function updateDisplay(display) {
             }
             break;
         case "todo":
-            while (newTodo.nextSibling) {
-                newTodo.nextSibling.remove();
+            while (newTodoButtonDiv.nextSibling) {
+                newTodoButtonDiv.nextSibling.remove();
+            }
+            break;
+        case "error":
+            const errorMessage = document.getElementById("error");
+            if (errorMessage) {
+                errorMessage.remove();
             }
             break;
     } 
@@ -43,38 +52,36 @@ function displayProjects() {
     })
 }
 
-function displayTodos(event) {
-    const target = event.target;
+function displayTodos() {
+    Projects.forEach((project) => {
+        if (project.isSelected === true) {
+            updateDisplay("todo");
 
-    if (target.className === "project") {
-        updateDisplay("todo");
+            project.todoItem.forEach((todo) => {
+                const todoItem = document.createElement("div");
+                const todoTitle = document.createElement("h3");
+                const todoDueDate = document.createElement("div");
 
-        const clickedProjectIndex = target.dataset.indexNumber;
+                todoTitle.textContent = todo.title;
+                todoDueDate.textContent = todo.dueDate.replace("T", " | ");
 
-        Projects[clickedProjectIndex].todoItem.forEach((item) => {
-            const todoItem = document.createElement("div");
-            const todoTitle = document.createElement("h3");
-            const todoDueDate = document.createElement("div");
+                todoItem.className = "todo-item";
 
-            todoTitle.textContent = item.title;
-            todoDueDate.textContent = item.dueDate;
-
-            todoItem.className = "todo-item";
-
-            todoSection.appendChild(todoItem);
-            todoItem.appendChild(todoTitle);
-            todoItem.appendChild(todoDueDate);
-        })
-    }
+                todoSection.appendChild(todoItem);
+                todoItem.appendChild(todoTitle);
+                todoItem.appendChild(todoDueDate);
+            })
+        }
+    })
 }
 
 sidebar.addEventListener("click", displayTodos);
 
 function addProject() {
-    const newProjectDialog = document.querySelector(".new-project");
-    const projectNameInput = document.querySelector("#project-name");
-    const addProjectButton = document.querySelector(".add-project");
-    const closeDialog = document.querySelector(".close");
+    const newProjectDialog = document.getElementById("new-project");
+    const projectNameInput = document.getElementById("project-name");
+    const addProjectButton = document.getElementById("add-project");
+    const closeDialog = document.getElementById("cancle-project");
 
     newProjectDialog.show();
  
@@ -82,11 +89,11 @@ function addProject() {
         if (projectNameInput.value !== "") {
             createProject(projectNameInput.value);
             newProjectDialog.close();
+
+            projectNameInput.value = "";
+
+            displayProjects();
         }
-
-        projectNameInput.value = "";
-
-        displayProjects();
     })
 
     closeDialog.addEventListener("click", () => {
@@ -95,10 +102,73 @@ function addProject() {
 }
 
 newProjectButton.addEventListener("click", addProject);
-    
-function testLog() {
-    console.log("");
+
+function addTodo() {
+    const newTodoDialog = document.getElementById("new-todo");
+    const titleInput = document.getElementById("title");
+    const descInput = document.getElementById("desc");
+    const dateInput = document.getElementById("dueDate");
+    const prioritySelect = document.getElementById("priority");
+    const addTodoButton = document.getElementById("add-todo");
+    const cancelButton = document.getElementById("cancel-todo");
+    const formattedCurrentDate = getCurrentDate();
+
+    dateInput.min = formattedCurrentDate;
+
+    newTodoDialog.show();
+
+    function inputCheck() {
+        if (titleInput.value === "" || descInput.value === ""
+            || dateInput.value === formattedCurrentDate) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function clearInputs() {
+        const inputs = document.querySelectorAll("#new-todo input");
+
+        inputs.forEach((input) => {
+            input.value = "";
+        })
+    }
+
+    addTodoButton.addEventListener("click", () => {
+        if (inputCheck()) {
+            updateDisplay("error");
+
+            const errorMessage = document.createElement("div");
+            errorMessage.textContent = "You need to fill in all the fields or set the due date correctly."
+            errorMessage.id = "error";
+
+            newTodoDialog.appendChild(errorMessage);
+        } else {
+            Projects.forEach((project) => {
+                if (project.isSelected === true) {
+                    project.createTodo(titleInput.value, descInput.value, dateInput.value,
+                                       prioritySelect.value);
+                }
+            })
+
+            displayTodos();
+            clearInputs();
+            
+            newTodoDialog.close();
+        }
+    })
+
+    cancelButton.addEventListener("click", () => {
+        newTodoDialog.close();
+    })
 }
 
-export { displayProjects, testLog };
+newTodoButton.addEventListener("click", addTodo);
+
+    
+function testLog() {
+    console.log((Projects));
+}
+
+export { displayProjects, testLog, displayTodos };
 
