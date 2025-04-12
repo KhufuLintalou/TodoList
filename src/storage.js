81723bdc341c0f5a6bf967d0070a449572764f8a
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2025-04-12 12:36:33 +0700

    Added a module for handling localStorage.

---
+++ src/storage.js
@@ -0,0 +1,26 @@
+import { Project } from "./todoList.js";
+
+export function saveProjects(projectArr) {
+    localStorage.setItem("Projects", JSON.stringify(projectArr));
+}
+
+export function addSavedProjects(projectArr) {
+    if (localStorage.getItem("Projects")) {
+        const savedProjects = Array.from(JSON.parse(localStorage.getItem("Projects")));
+        
+        savedProjects.forEach((project) => {
+            const newProject = new Project(project.name);
+            
+            if (project.isSelected === true) {
+                newProject.selectProject();
+            }
+
+            projectArr.push(newProject);
+
+            project.todoItem.forEach((todo) => {
+                newProject.createTodo(todo.title, todo.descript, todo.dueDate,
+                                      todo.priority, todo.category, todo.status);
+            })
+        })
+    }
+}
